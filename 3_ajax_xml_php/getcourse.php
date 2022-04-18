fd5d3dc6595a6dd1955fa95285b4f206987ef50d
MyintLwanCoder <myintlwancoder2021@gmail.com>
2022-04-18 10:23:48 +0630

    third_update

---
+++ 3_ajax_xml_php/getcourse.php
@@ -0,0 +1,20 @@
+<?php
+   $q = $_GET["q"];
+   $xmlDoc = new DOMDocument();
+   $xmlDoc->load("courses.xml");
+   $x = $xmlDoc->getElementsByTagName('COURSE');
+   for ($i = 0; $i<=$x->length-1; $i++) {
+      if ($x->item($i)->nodeType == 1) {
+         if ($x->item($i)->childNodes->item(0)->nodeValue == $q) {
+            $y = ($x->item($i)->parentNode);
+         }
+      }
+   }
+   $cd = ($y->childNodes);
+   for ($i = 0;$i<$cd->length;$i++) {
+      if ($cd->item($i)->nodeType == 1) {
+         echo("" . $cd->item($i)->nodeName . ": ");
+         echo($cd->item($i)->childNodes->item(0)->nodeValue);
+         echo("");
+      }
+   }
---
+++ 3_ajax_xml_php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+ <script src="app.js"></script>
+</head>
+<body>
+ <form>
+Select a Course:    <select name="cds" >
+    <option value="">Select a course:</option>
+    <option value="Python">Python</option>
+    <option value="PHP">PHP</option>
+    <option value="JavaScript">JavaScript</option>
+    <option value="GO">GO</option>
+   </select>
+  </form>
+  <div id="txtHint"><b>Course info will be listed here...</b></div>
+</body>
+</html>
